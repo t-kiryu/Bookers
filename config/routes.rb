e82b28970f063d9fd98671b9af968b5f2d923a4a
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-05-25 12:09:35 +0000

    [Start]bookers

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id' => 'books#show'
+
+  get 'books/edit'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
